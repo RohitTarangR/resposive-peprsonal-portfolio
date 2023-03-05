32c702cc404ce5391fe8e9f8ae488f4dc849dda3
RohitTarangR <rohittarang512005@gmail.com>
2023-03-05 14:42:03 +0530

    portfolio website

---
+++ testimonials.js
@@ -0,0 +1,26 @@
+const testimonials = [
+    {
+        quote: "Our company, we do ongoing research with our target audience. This includes 38-45 minutes phone interview. It was difficult to conduct the interview, really listen and ask good follow up questions.",
+        avatar: "client1",
+        name: "Diana Ayi",
+        title: "Musician"
+    },
+    {
+        quote: "Our company, we do ongoing research with our target audience. This includes 38-45 minutes phone interview. It was difficult to conduct the interview, really listen and ask good follow up questions.",
+        avatar: "client2",
+        name: "Diana Ayi",
+        title: "Musician"
+    },
+    {
+        quote: "Our company, we do ongoing research with our target audience. This includes 38-45 minutes phone interview. It was difficult to conduct the interview, really listen and ask good follow up questions.",
+        avatar: "client3",
+        name: "Diana Ayi",
+        title: "Musician"
+    },
+    {
+        quote: "Our company, we do ongoing research with our target audience. This includes 38-45 minutes phone interview. It was difficult to conduct the interview, really listen and ask good follow up questions.",
+        avatar: "client4",
+        name: "Diana Ayi",
+        title: "Musician"
+    },
+]
